252298ce27ba8c905188006424f7eafbfa83eb79
Álvaro <alvaro1997rx@gmail.com>
2025-07-21 19:10:37 +0200

    Add files via upload

---
+++ Js/Script.js
@@ -0,0 +1,6 @@
+function enviarFormulario(){
+        alert ("¡Tu pedido ha sido enviado con éxito");
+    }
+    function resetFormulario(){
+        alert ("El formulario ha sido reseteado");
+    }
